7009589857a7122a6551b825176af07c82d52f38
Nicolas Falciglio <136000929+nicofal23@users.noreply.github.com>
2023-12-06 16:20:52 +0000

    agregue para descontar stock cuando el cliente compra

--- src/components/Order/Order.jsx
+++ src/components/Order/Order.jsx
@@ -1,11 +1,10 @@
-// Order.jsx
 import React, { useContext, useState } from 'react';
 import Swal from 'sweetalert2';
 import withReactContent from 'sweetalert2-react-content';
 import { CartContext } from '../../context/CartContext';
 import styles from './Order.module.css';
 import { db } from '../../firebase/cliente';
-import { addDoc, collection, serverTimestamp } from 'firebase/firestore';
+import { addDoc, collection, serverTimestamp, getDoc, doc, updateDoc } from 'firebase/firestore';
 
 const MySwal = withReactContent(Swal);
 
@@ -16,7 +15,6 @@ const Order = () => {
     phone: '',
     email: '',
   });
-  const [orderNumber, setOrderNumber] = useState(null);
 
   const handleInputChange = (e) => {
     const { name, value } = e.target;
@@ -45,23 +43,43 @@ const Order = () => {
       return;
     }
 
-    const order = {
-      buyer: customerInfo,
-      items: cart.map((item) => ({
-        id: item.item.id,
-        nombre: item.item.nombre,
-        cantidad: item.cantidad,
-        precio: item.item.precio,
-      })),
-      total: cart.reduce((acc, curr) => acc + curr.item.precio * curr.cantidad, 0),
-    };
-
     try {
+      // Crear la orden en la colección 'orders'
+      const order = {
+        buyer: customerInfo,
+        items: cart.map((item) => ({
+          id: item.item.id,
+          nombre: item.item.nombre,
+          cantidad: item.cantidad,
+          precio: item.item.precio,
+        })),
+        total: cart.reduce((acc, curr) => acc + curr.item.precio * curr.cantidad, 0),
+      };
+
       const docRef = await addDoc(collection(db, 'orders'), {
         ...order,
         createdAt: serverTimestamp(),
       });
 
+      // Reducir la cantidad de productos en stock
+      for (const item of cart) {
+        const productDocRef = doc(db, 'productos', item.item.id);
+
+        // Obtener el valor actual del stock
+        const productSnap = await getDoc(productDocRef);
+        const currentStock = productSnap.data().stock;
+
+        // Verificar que hay suficiente stock antes de actualizar
+        if (currentStock >= item.cantidad) {
+          await updateDoc(productDocRef, {
+            stock: currentStock - item.cantidad,
+          });
+        } else {
+          console.error(`Error al reducir el stock para el producto ${item.item.id}. Stock actual: ${currentStock}, Cantidad comprada: ${item.cantidad}`);
+        }
+      }
+
+      // Limpiar el carrito después de realizar la compra
       clearCart();
 
       MySwal.fire({
@@ -130,11 +148,10 @@ const Order = () => {
               Comprar
             </button>
           </form>
-          {orderNumber && <p>Número de Orden: {orderNumber}</p>}
         </div>
       </div>
     </div>
   );
-}; 
+};
 
 export default Order;
